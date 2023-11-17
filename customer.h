00ea29a538fbf2041c916f45236ebbe698fcaa45
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2023-11-17 10:54:26 +0000

    added CustomerCounter class

--- customer.h
+++ customer.h
@@ -1,4 +1,17 @@
 #ifndef _CUSTOMER_H_
 #define _CUSTOMER_H_
 
+#include <string>
+
+class CustomerCounter {
+    private:
+        int maximum_customers;
+    public:
+        CustomerCounter(int maximum_customers);
+        int customer_count = 0;
+        int add();
+        int subtract();
+};
+
+
 #endif
